15dedb2606cc2b37feead4975b220f059107e281
codeformints <sha44489@gmail.com>
2025-03-23 15:00:25 -0500

    Added dark mode and audio guest book project page

---
+++ javascript/dark-mode.js
@@ -0,0 +1,33 @@
+const changeButtonTheme = () => {
+    var theme = localStorage.getItem('theme');
+    if(theme == 'light')
+    {
+        document.getElementById('darkmodetoggle').innerHTML = '<i style="padding-top: 2%;" class="bi bi-moon"></i>'
+    }
+    else
+    {
+        document.getElementById('darkmodetoggle').innerHTML = '<i style="padding-top: 2%;" class="bi bi-sun"></i>'
+    }
+}
+
+const toggleTheme = () => {
+    var theme = localStorage.getItem('theme')
+    if(theme == 'light')
+    {
+        localStorage.setItem('theme', 'dark')
+        document.documentElement.setAttribute('data-bs-theme', localStorage.getItem('theme'))
+        changeButtonTheme()
+    }
+    else
+    {
+        localStorage.setItem('theme', 'light')
+        document.documentElement.setAttribute('data-bs-theme', localStorage.getItem('theme'))
+        changeButtonTheme()
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    //localStorage.setItem('theme', 'dark')
+    document.documentElement.setAttribute('data-bs-theme', localStorage.getItem('theme'))
+    changeButtonTheme()
+})
