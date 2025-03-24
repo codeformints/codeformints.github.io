e2ba485374f34785c9acefb61cf8523f67c329f9
codeformints <sha44489@gmail.com>
2025-03-24 13:09:01 -0500

    fixed projects page

--- javascript/dark-mode.js
+++ javascript/dark-mode.js
@@ -2,32 +2,38 @@ const changeButtonTheme = () => {
     var theme = localStorage.getItem('theme');
     if(theme == 'light')
     {
-        document.getElementById('darkmodetoggle').innerHTML = '<i style="padding-top: 2%;" class="bi bi-moon"></i>'
+        document.getElementById('darkmodetoggle').innerHTML = '<i style="padding-top: 2%;" class="bi bi-moon"></i>';
     }
     else
     {
-        document.getElementById('darkmodetoggle').innerHTML = '<i style="padding-top: 2%;" class="bi bi-sun"></i>'
+        document.getElementById('darkmodetoggle').innerHTML = '<i style="padding-top: 2%;" class="bi bi-sun"></i>';
     }
 }
 
 const toggleTheme = () => {
-    var theme = localStorage.getItem('theme')
+    var theme = localStorage.getItem('theme');
     if(theme == 'light')
     {
-        localStorage.setItem('theme', 'dark')
-        document.documentElement.setAttribute('data-bs-theme', localStorage.getItem('theme'))
-        changeButtonTheme()
+        localStorage.setItem('theme', 'dark');
+        document.documentElement.setAttribute('data-bs-theme', localStorage.getItem('theme'));
+        changeButtonTheme();
     }
     else
     {
-        localStorage.setItem('theme', 'light')
-        document.documentElement.setAttribute('data-bs-theme', localStorage.getItem('theme'))
-        changeButtonTheme()
+        localStorage.setItem('theme', 'light');
+        document.documentElement.setAttribute('data-bs-theme', localStorage.getItem('theme'));
+        changeButtonTheme();
     }
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    //localStorage.setItem('theme', 'dark')
-    document.documentElement.setAttribute('data-bs-theme', localStorage.getItem('theme'))
-    changeButtonTheme()
+    var theme = localStorage.getItem('theme');
+
+    if(theme != 'dark' && theme != 'light')
+    {
+        localStorage.setItem('theme', 'light');
+    }
+
+    document.documentElement.setAttribute('data-bs-theme', localStorage.getItem('theme'));
+    changeButtonTheme();
 })
